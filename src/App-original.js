e156e0462f661cca79a85ea9dfe2914a5b721c71
Muhammad Ahmad <ranaahmad131@gmail.com>
2024-08-05 17:53:28 +0500

    Working on fixing mapping

--- src/App-original.js
+++ src/App-original.js
@@ -12,26 +12,23 @@ function App() {
     <PackagingList />
     <Footer/>
   </div>
-  
-  
   )
-
-
-
 }
 
 function Logo(){
   return <header>
-    <h1>Logo
+    <h1>
+      CheckList
     </h1>
     </header>
 }
 
 
 function Form() {
+  const [items, setItems] = useState([])
   const [num, setNum] = useState("")
   const [name, setName] = useState("")
-  const [item, setItem] = useState([])
+
 
   function handleQuantity(e){
     setNum(e.target.value)
@@ -41,23 +38,42 @@ function Form() {
     setName(e.target.value)
   }
 
-  return <form className="add-form">
-    <input type="number" value={num} onChange={handleQuantity}/>
-    <input type="text" value={name} onChange={handleName}/>
-  </form>
+  function handleSubmit(e){
+    e.preventDefault()
+    if(!name.length || isNaN(num)) return 
+    setItems([...items, {itemName: name, quantity: num}])
+    setNum("")
+    setName("")
+  }
+  // console.log(name,num, items)
+
+  return <>   
+    <form className="add-form" onSubmit={handleSubmit}>
+      <input type="number" value={num} onChange={handleQuantity} placeholder="Enter Number" />
+      <input type="text" value={name} onChange={handleName} placeholder="Enter Name" />
+      <button type="submit">ADD</button>
+    </form>
+    <PackagingList items={items}/>
+  </>
 } 
 
 
-function PackagingList() {
-  return <header>Logo</header>
-} 
-
-
-function List() {
-  return <header>Logo</header>
+function PackagingList({ items }) {
+  console.log(items);
+  return (
+   <div className="list">
+      <ul>
+        {items.map((item) => (
+          <li>{item}</li>
+        ))}
+      </ul>
+   </div>
+  );
 }
 
-
+// function List({ name, qty }) {
+//   return <li>{name}~{qty}</li>;
+// }
 
 function Footer() {
   return <footer>Logo</footer>
