fa9b3f4b2283229f2bbe91bdc4d3743dfcd0265d
Muhammad Ahmad <ranaahmad131@gmail.com>
2024-08-06 18:37:35 +0500

    Working on total checked

--- src/App-original.js
+++ src/App-original.js
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import PackingLists from "./components/PackingLists";
 
 
 
@@ -9,7 +10,7 @@ function App() {
   <div className="app">
     <Logo/>
     <Form />
-    <PackagingList />
+    {/* <PackagingList /> */}
     <Footer/>
   </div>
   )
@@ -28,10 +29,10 @@ function Form() {
   const [items, setItems] = useState([])
   const [num, setNum] = useState("")
   const [name, setName] = useState("")
-
+const code = Date.now()
 
   function handleQuantity(e){
-    setNum(e.target.value)
+    setNum(Number(e.target.value))
   }
 
   function handleName(e){
@@ -40,43 +41,64 @@ function Form() {
 
   function handleSubmit(e){
     e.preventDefault()
-    if(!name.length || isNaN(num)) return 
-    setItems([...items, {itemName: name, quantity: num}])
+    setItems([...items, {itemName: name, quantity: num, id: code}])
     setNum("")
     setName("")
   }
-  // console.log(name,num, items)
-
-  return <>   
+console.log(items)
+  return <div>   
     <form className="add-form" onSubmit={handleSubmit}>
       <input type="number" value={num} onChange={handleQuantity} placeholder="Enter Number" />
       <input type="text" value={name} onChange={handleName} placeholder="Enter Name" />
-      <button type="submit">ADD</button>
+      <button type="submit" >ADD</button>
     </form>
     <PackagingList items={items}/>
-  </>
-} 
-
+  </div>
+  
+}
 
 function PackagingList({ items }) {
-  console.log(items);
+
   return (
-   <div className="list">
-      <ul>
-        {items.map((item) => (
-          <li>{item}</li>
-        ))}
-      </ul>
-   </div>
+    <ul className="list">
+      {items.map((x, index) => (
+        // <li key={index} id={x.id} className={`${checked? "underline" : ""}`}>
+        //   <input type="checkbox" onSelect={handleChecked}/>
+        //   {x.itemName} - {x.quantity}
+        // </li>
+
+        <Lst name={x.itemName} key={x.id} quantity={x.quantity} id={x.id}/>
+      ))}
+    </ul>
   );
 }
 
-// function List({ name, qty }) {
-//   return <li>{name}~{qty}</li>;
+function Lst({name, quantity, id}){
+  const [checked, setChecked] = useState(false);
+  // const [total, setTotal] = useState('')
+ 
+//   function totalQty(){
+//     if(quantity> 0){
+//     let sum = quantity.reduce((x,y)=> x+y, 0)
+//     setTotal(sum)
 // }
+//   }
 
-function Footer() {
-  return <footer>Logo</footer>
+  function handleChecked() {
+    setChecked(!checked)
+  }
+  return <>
+    <li className={`${checked ? "underlined" : ""}`}>
+      <input type="checkbox" onChange={handleChecked} />
+      {name} - {quantity}
+    </li>
+    <Footer  />
+    </>
+}
+
+
+function Footer({packed}) {
+  return <footer className="stats">You have {packed} packed items.</footer>
 }
 
 
