a5433ec3baf4850556ca2accc8d6d826374e0a7d
Muhammad Ahmad <ranaahmad131@gmail.com>
2024-08-16 16:19:07 +0500

    Working on making it functional

--- src/App-original.js
+++ src/App-original.js
@@ -1,15 +1,39 @@
-import React from "react";
+import React, { useState } from "react";
 
 
 
 function App() {
+  const [items, setItem] = useState([])
+  const [num, setNum] = useState("");
+  const [name, setName] = useState("")
 
+
+  function handleSubmission(e) {
+    e.preventDefault()
+    setItem([...items, { name: name, num: num, id: Date.now() }])
+    setName("")
+    setNum("")
+  }
+
+  function handleNum(e) {
+    setNum(e.target.value)
+
+  }
+
+  function handleName(e) {
+    setName(e.target.value)
+  }
+
+  function handleDel(id){
+    // console.log(id)
+    setItem(items=>items.filter(item=>item.id !== id))
+  }
   return (
   
   <div className="app">
     <Logo/>
-    <Form />
-    <PackagingList />
+      <Form onSubmit={handleSubmission} name={name} num={num} onAddNum={handleNum} onAddName={handleName} />
+    <PackagingList items={items} onDel={handleDel}/>
     <Footer/>
   </div>
   
@@ -28,28 +52,45 @@ function Logo(){
 }
 
 
-function Form() {
-  return <form className="add-form">
+function Form({ onSubmit, onAddName, onAddNum, num, name}) {
+  return <form className="add-form" onSubmit={onSubmit}>
 
-    <input type="number"/>
-    <input type="text" />
+    <input type="number" placeholder="Enter Number" value={num} onChange={onAddNum} />
+    <input type="text" placeholder="Enter Name" value={name} onChange={onAddName} />
+    <button type="submit">Add</button>
   </form>
 } 
 
 
-function PackagingList() {
-  return <header>Logo</header>
+function PackagingList({items, onDel}) {
+
+  return (
+    <ul className="list">
+      {items.map((item) => (
+        <List name={item.name} onDel={onDel} key={item.id} id={item.id} num={item.num}/>
+      ))}
+    </ul>
+  )
 } 
 
 
-function List() {
-  return <header>Logo</header>
+function List({name, num, onDel,id}) {
+
+
+
+  return <li className="">
+    <span>
+      <input type="checkbox" />
+      {num} - {name}
+    </span>
+    <button className="close-button" onClick={()=>onDel(id)}>X</button>
+  </li>
 }
 
 
 
 function Footer() {
-  return <footer>Logo</footer>
+  return <footer className="stats">Total number of items: ${""} </footer>
 }
 
 
