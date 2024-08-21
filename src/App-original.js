8a0cd2a39a2cd10f1ffcebfb08bc3298e2ec019b
Muhammad Ahmad <ranaahmad131@gmail.com>
2024-08-21 19:33:19 +0500

    Fixed packed funtionality

--- src/App-original.js
+++ src/App-original.js
@@ -1,130 +1,122 @@
 import React, { useState } from "react";
 
-
-
 function App() {
-  const [items, setItem] = useState([])
+  const [items, setItem] = useState([]);
   const [num, setNum] = useState("");
-  const [name, setName] = useState("")
-  const [packed, setPacked] = useState(false)
+  const [name, setName] = useState("");
 
   function itemPacked(id) {
-    console.log(id)
-    setPacked(packed => {
-      const newPacked = !packed;
-      setItem(items =>
-        items.map(item =>
-          item.id === id ? { ...item, packed: newPacked } : item
-        )
-      );
-      return newPacked;
-    }) 
-     
+    setItem(items =>
+      items.map(item =>
+        item.id === id ? { ...item, packed: !item.packed } : item
+      )
+    );
   }
-console.log(items)
 
-  const totalItems = Number(items.reduce((x, y) => x + y.num, 0))
-  const packeditems = items.filter(item=> item.packed? item.packed :"")
-  console.log(packeditems)
+  const totalItems = items.reduce((x, y) => x + Number(y.num), 0);
   const totalPackedItems = items.reduce((total, item) => {
-    return item.packed ? total + item.num : total;
+    return item.packed ? total + Number(item.num) : total;
   }, 0);
-  // const checking = items.map((item)=> item.packed? item.num: "")
-  // const totalPackedItems =checking.reduce((a,b)=> a + b.num,0)
-  // console.log(totalPackedItems)
 
-function handleSubmission(e) {
-    e.preventDefault()
-    setItem([...items, { name: name, num: num, id: Date.now() }])
-    setName("")
-    setNum("")
+  function handleSubmission(e) {
+    e.preventDefault();
+const existingItem = items.some(item=>item.name.toLowerCase() === name.toLowerCase() );
+if (existingItem) {
+  alert("Item already exists");
+}
+else {
+  setItem([...items, { name: name, num: Number(num), id: Date.now(), packed: false }]);
+}
+    setName("");
+    setNum("");
   }
 
   function handleNum(e) {
-    setNum(Number(e.target.value))
-
+    setNum(e.target.value);
   }
 
   function handleName(e) {
-    setName(e.target.value)
+    // if(items.name.includes(e.target.value)) return
+    setName(e.target.value);
   }
 
-  function handleDel(id){
-    setItem(items=>items.filter(item=>item.id !== id))
+  function handleDel(id) {
+    setItem(items => items.filter(item => item.id !== id));
   }
-  function handleToggle(id) {
-    setItem(items =>
-      items.map(item =>
-        item.id === id ? { ...item, packed: packed} : item
-      )
-    );
-    console.log(id)
-  }
-
-
 
   return (
-  <div className="app">
-    <Logo/>
+    <div className="app">
+      <Logo />
       <Form onSubmit={handleSubmission} name={name} num={num} onAddNum={handleNum} onAddName={handleName} />
-      <PackagingList items={items} packed={packed} onDel={handleDel} onToggleItem={handleToggle} onTgl={itemPacked}/>
-      <Footer total={totalItems} totalPackedItems={totalPackedItems}/>
-  </div>
-  )
+      <PackagingList items={items} onDel={handleDel} onToggleItem={itemPacked} />
+      <Footer total={totalItems} totalPackedItems={totalPackedItems} />
+    </div>
+  );
 }
 
-function Logo(){
-  return <header>
-    <h1>
-      CheckList
-    </h1>
+function Logo() {
+  return (
+    <header>
+      <h1>CheckList</h1>
     </header>
+  );
 }
 
+function Form({ onSubmit, onAddName, onAddNum, num, name }) {
+  return (
+    <form className="add-form" onSubmit={onSubmit}>
+      <input type="number" placeholder="Enter Number" value={num} onChange={onAddNum} />
+      <input type="text" placeholder="Enter Name" value={name} onChange={onAddName} />
+      <button type="submit">Add</button>
+    </form>
+  );
+}
 
-function Form({ onSubmit, onAddName, onAddNum, num, name}) {
-  return <form className="add-form" onSubmit={onSubmit}>
-
-    <input type="number" placeholder="Enter Number" value={num} onChange={onAddNum} />
-    <input type="text" placeholder="Enter Name" value={name} onChange={onAddName} />
-    <button type="submit">Add</button>
-  </form>
-} 
-
-
-function PackagingList({ items, onDel, onToggleItem, onTgl, packed }) {
-
+function PackagingList({ items, onDel, onToggleItem }) {
   return (
     <ul className="list">
-      {items.map((item) => (
-        <List name={item.name} onTgl={onTgl} packed={packed} item={item} onToggleItem={onToggleItem} onDel={onDel} key={item.id} id={item.id} num={item.num}/>
+      {items.map(item => (
+        <List
+          name={item.name}
+          packed={item.packed}
+          item={item}
+          onToggleItem={onToggleItem}
+          onDel={onDel}
+          key={item.id}
+          id={item.id}
+          num={item.num}
+        />
       ))}
     </ul>
-  )
-} 
-
-
-function List({ item, name, num, onDel, onToggleItem, id, onTgl, packed }) {
-
-
-
-  return <li className="">
-    <span>
-      <input type="checkbox" value={packed} onChange={() => onTgl(id)}/>
-      {num} - {name}
-    </span>
-    <button className="close-button" onClick={()=>onDel(id)}>X</button>
-  </li>
+  );
 }
 
-
+function List({ item, name, num, onDel, onToggleItem, id, packed }) {
+  return (
+    <li className="">
+      <span>
+        <input
+          type="checkbox"
+          checked={packed}
+          onChange={() => onToggleItem(id)}
+        />
+        {num} - {name}
+      </span>
+      <button className="close-button" onClick={() => onDel(id)}>
+        X
+      </button>
+    </li>
+  );
+}
 
 function Footer({ total, totalPackedItems }) {
-  console.log(totalPackedItems)
-  return <footer className="stats">
-    {total > 0 ? `You have total ${total} items and  you have packed ${totalPackedItems} items` : "Please start adding items"}
-     </footer>
+  return (
+    <footer className="stats">
+      {total > 0
+        ? `You have a total of ${total} items and you have packed ${totalPackedItems} items`
+        : "Please start adding items"}
+    </footer>
+  );
 }
-
 
 export default App;
