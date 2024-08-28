e0844ca039f9a354f75cba99af848796158aa34d
Muhammad Ahmad <ranaahmad131@gmail.com>
2024-08-28 13:55:21 +0500

    Completed the quiz

--- src/components/Quiz1.1/Quiz.js
+++ src/components/Quiz1.1/Quiz.js
@@ -1,3 +1,4 @@
+import { useState } from "react"
 import "../Quiz1.1/style.css"
 
 
@@ -10,34 +11,68 @@ function Quiz(){
 }
 
 function Form(){
+const [amount, setAmount] = useState("")
+const [myExp, setMyExp] = useState("null")
+const [frenExp, setFrenExp] = useState("null")
 
 
-    return <form className="">
+    function handleAmount(e){
+        if (e.target.value <= 0 || isNaN(e.target.value)) return
+        setAmount(Number(e.target.value))
+        console.log(amount)
+    }
+
+    function handleMine(e) {
+        setMyExp(e.target.value)
+    }
+    function handleFren(e) {
+        setFrenExp(e.target.value)
+    }
+
+    let exp;
+    if (myExp === "okay") exp = amount;
+    if (myExp === "good")  exp = (amount * 10)/100 ;
+    if (myExp === "excellent")  exp = (amount * 20) / 100 ;
+   
+    let frexp;
+    if (frenExp === "okay") frexp = amount;
+    if (frenExp === "good") frexp = (amount * 10) / 100 ;
+    if (frenExp === "excellent") frexp = (amount * 20) / 100 ;
+
+
+    return <div className="">
         <label>How much was the bill?</label>
-        <input type="number" placeholder="enter amount" />
+        <input type="number" value={amount} onChange={handleAmount} placeholder="enter amount" />
             <br></br>
+
         <label>How did you like the service?</label>
-        <select >
+        <select value={myExp} onChange={handleMine}>
             <option value="okay">Okay</option>
             <option value="good">Good</option>
             <option value="excellent">Excellent</option>
         </select>
+
+
         <br></br>
         <label>How did your friend like the service?</label>
-        <select >
+        <select onChange={handleFren} value={frenExp}>
             <option value="okay">Okay</option>
             <option value="good">Good</option>
             <option value="excellent">Excellent</option>
         </select>
-    </form>
-}
 
-
-function Display(){
-    return <div className="display">
-        <h3>Your bill:</h3>
+        {amount && <div>
+            <p>Your bill : `{amount}$`</p>
+            <p>Your tip : `{exp}$`</p>
+            <p>Your Friend tip : `{frenExp}$`</p>
+            <p>Total Paid: `{amount + exp + frenExp}$`</p>
+        </div>}
     </div>
+
 }
+
+
+
 
 
 
