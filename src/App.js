acfaa623ae4e6a11e99e31df03ca96ff6630726d
Muhammad Ahmad <133768693+muhammaddahmadd@users.noreply.github.com>
2025-07-21 23:57:29 +0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -14,7 +14,6 @@ function App() {
   function handleDel(id) {
     setItems(() => items.filter((item) => item.id !== id));
   }
-
   function toggleItem(id) {
     setItems((items) =>
       items.map((item) =>
